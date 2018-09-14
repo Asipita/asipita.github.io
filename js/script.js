4d704e10f7d521c6d46a68aa94b47d835a2534a1
Asipita <sh.abdulgafar@gmail.com>
2018-09-14 11:32:06 +0100

    checking out github pages

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-$('.parallax').parallax();
-$('.modal').modal();
-$('.sidenav').sidenav();
-// $('select').formSelect();
-// CKEDITOR.replace('body');
